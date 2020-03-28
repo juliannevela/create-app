dbe25adac5b0803eeb802bd7073af1303530d278
Ryan Mehta <ryan.mehta@gmail.com>
2020-03-28 12:48:09 -0700

    initial setup

---
+++ index.js
@@ -0,0 +1,56 @@
+const { program } = require('commander');
+const { prompt } = require('inquirer');
+const nodeSetup = require('./lib/nodeSetup');
+const reactSetup = require('./lib/reactSetup');
+const basicSetup = require('./lib/basicSetup');
+
+const scripts = {
+  node: nodeSetup,
+  react: reactSetup,
+  basic: basicSetup
+};
+
+program
+  .option('-t, --type <type>', 'application type node, react, or basic')
+  .option('-n, --name <name>', 'application name')
+  .parse(process.argv);
+
+getConfig(program)
+  .then(init);
+
+async function init(config) {
+  return scripts[config.type](config);
+}
+
+async function getConfig(program) {
+  return program.type && program.applicationFolder
+    ? {
+      type: program.type,
+      applicationFolder: program.args[0],
+      appName: program.name || program.args[0]
+    }
+    : prompt([
+      {
+        type: 'list',
+        name: 'type',
+        message: 'Application Type',
+        choices: [
+          'node',
+          'react',
+          'basic'
+        ]
+      },
+      {
+        type: 'input',
+        name: 'applicationFolder',
+        message: 'Application Folder (. for current folder)',
+        default: 'alchemy-app'
+      },
+      {
+        type: 'input',
+        name: 'appName',
+        message: 'Application Name',
+        default: 'alchemy-app'
+      }
+    ]);
+}
