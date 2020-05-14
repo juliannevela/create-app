991c5727133f3612682cfece4f31139b81d71865
Ryan Mehta <ryan.mehta@gmail.com>
2020-05-14 08:30:07 -0700

    fix node setup

--- node/lib/app.js
+++ node/lib/app.js
@@ -3,8 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api/v1/reports', require('./routes/reports'));
-app.use('/api/v1/runs', require('./routes/runs'));
+app.use('/api/v1/RESOURCE', require('./routes/RESOURCE'));
 
 app.use(require('./middleware/not-found'));
 app.use(require('./middleware/error'));
